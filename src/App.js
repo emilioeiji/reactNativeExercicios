b6569c005ab5f6059bc49f404da54853f8820191
emilioeiji <emilioeiji@gmail.com>
2023-03-03 08:49:39 +0900

    #24 Componentes com propriedades.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
+/* import Oficinal, { Comp1, Comp2 } from './components/Multi'
+import Primeiro from './components/Primeiro' */
+import MinMax from './components/MinMax'
 
-export default () => <Text>Componente Teste</Text>
+export default () => (
+    <View style={style.App}>
+        <MinMax min='3' max='20' />
+        <MinMax min='1' max='94' />
+        {/* <Oficinal />
+        <Comp1 />
+        <Comp2 />
+        <Primeiro /> */}
+    </View>
+    )
+
+    const style = StyleSheet.create({
+        App: {
+            flexGrow: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 20
+        }
+    })
