43d2a4c610e5eac0b84589b13e97cf0efb39e6f3
emilioeiji <emilioeiji@gmail.com>
2023-03-03 11:47:10 +0900

    Teste tela de login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
-/* import Oficinal, { Comp1, Comp2 } from './components/Multi'
-import Primeiro from './components/Primeiro' */
-import MinMax from './components/MinMax'
+// import Oficinal, { Comp1, Comp2 } from './components/Multi'
+// import Primeiro from './components/Primeiro'
+// import MinMax from './components/MinMax'
+import Aleatorio from './components/Aleatorio'
+import Login from './components/Login'
 
 export default () => (
     <View style={style.App}>
+        <Login />
+        {/* <Aleatorio min={1} max={60}/>
         <MinMax min='3' max='20' />
         <MinMax min='1' max='94' />
-        {/* <Oficinal />
+        <Oficinal />
         <Comp1 />
         <Comp2 />
         <Primeiro /> */}
@@ -18,9 +22,9 @@ export default () => (
 
     const style = StyleSheet.create({
         App: {
-            flexGrow: 1,
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: 20
+            flex: 1,
+            //justifyContent: 'center',
+            //alignItems: 'center',
+            //padding: 20
         }
     })
---
+++ src/components/Login.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Image,
+} from 'react-native';
+
+const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // TODO: handle login logic
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require('./images/avatar.jpg')}
+        />
+      </View>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={setUsername}
+          value={username}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+        />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 50,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  form: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 3,
+    width: '80%',
+    alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 15,
+    marginBottom: 20,
+    width: '100%',
+  },
+  button: {
+    backgroundColor: '#2e86de',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    width: '100%',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
+
+export default LoginScreen;
